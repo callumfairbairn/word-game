1e55b5a89ef5eb5065b9c64ebcaaab145a257179
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-06-24 22:11:45 +0100

    Add gride

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Grid} from "./Grid/Grid";
 
 function App() {
   return (
     <div className="App">
-
+      <Grid xDim={3} yDim={3} />
     </div>
   );
 }
---
+++ src/Grid/Grid.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Grid.scss'
+import {Square} from "../Square/Square";
+
+export const Grid = ({xDim, yDim}) => {
+    return (
+        <div className='grid'>
+            {Array.from(Array(yDim), (_,y) =>
+                <div className='row' key={y}>
+                    {Array.from(Array(xDim), (_,x) =>
+                        <div className='column' key={x}>
+                            <Square key={[x, y]} selected={false}/>
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
+    )
+};
+
