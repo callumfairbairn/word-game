3e9784fa9a7fdb96b4c4a53b5c459a9c0c0d261c
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-06-24 23:02:54 +0100

    Allow variable letters as input to squares

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -3,18 +3,33 @@ import './Grid.scss'
 import {Square} from "../Square/Square";
 
 export const Grid = ({xDim, yDim}) => {
+    const letterGrid = generateLetterGrid(xDim, yDim);
     return (
         <div className='grid'>
             {Array.from(Array(yDim), (_,y) =>
                 <div className='row' key={y}>
                     {Array.from(Array(xDim), (_,x) =>
                         <div className='column' key={x}>
-                            <Square key={[x, y]} selected={false} letter={'A'} />
+                            <Square key={[x, y]} selected={false} letter={letterGrid[x][y]} />
                         </div>
                     )}
                 </div>
             )}
         </div>
     )
+};
+
+const generateLetterGrid = (xDim, yDim) => {
+    let letters = Array.from(Array(xDim), () => {return Array.from(Array(yDim), () => {return ''})});
+    letters[0][0] = 'A';
+    letters[0][1] = 'B';
+    letters[0][2] = 'C';
+    letters[1][0] = 'D';
+    letters[1][1] = 'E';
+    letters[1][2] = 'F';
+    letters[2][0] = 'G';
+    letters[2][1] = 'H';
+    letters[2][2] = 'I';
+    return letters;
 };
 
