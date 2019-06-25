a6c4b5d79b9280a34d0a26a2e5f37ccf55ade062
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-06-25 19:01:25 +0100

    Add InputField component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Grid} from "./Grid/Grid";
+import {InputField} from "./InputField/InputField";
 
 function App() {
   return (
     <div className="App">
       <Grid xDim={4} yDim={4} />
+      <InputField/>
     </div>
   );
 }
---
+++ src/InputField/InputField.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const InputField = () => {
+  return (
+      <form className={'inputField'}>
+          <input className={'input'} type="text"/>
+      </form>
+  )
+};
