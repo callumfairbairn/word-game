c12a811418ec1892c3ce9fdc2244fdf667dcad79
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-06-26 22:33:38 +0100

    Allow input to send value to function

--- src/InputField/InputField.js
+++ src/InputField/InputField.js
@@ -3,7 +3,11 @@ import React from 'react'
 export const InputField = () => {
   return (
       <form className={'inputField'}>
-          <input className={'input'} type="text"/>
+          <input className={'input'} id='input' type="text" onInput={sendValue} />
       </form>
   )
+};
+
+const sendValue = () => {
+    console.log(document.getElementById('input').value);
 };
