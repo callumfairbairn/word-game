ea376e91d65d6916d7b880da62f303c89792d007
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-06-29 16:18:23 +0100

    Added reactTestSetup and testid for squares

--- src/Square/Square.js
+++ src/Square/Square.js
@@ -3,7 +3,7 @@ import './Square.scss'
 
 export const Square = ({selected, letter}) => {
     return (
-        <div className='square' id={`${selected && 'selected'}`}>
+        <div className='square' id={`${selected ? 'selected' : ''}`} data-testid={`square${selected ? '-selected' : ''}`}>
             <div className='letter'>
                 {letter}
             </div>
--- src/Square/Square.test.js
+++ src/Square/Square.test.js
@@ -1,10 +1,21 @@
 import React from "react";
 import {Square} from "./Square";
 import {render} from "@testing-library/react";
+import '../reactTestSetup';
 
 describe('Square', () => {
     it('displays a given letter', () => {
-        const { getByText } = render(<Square letter='A' />);
+        const { getByText } = render(<Square letter='A' selected={false} />);
         expect(getByText('A'))
+    });
+
+    it ('displayes an unselected square', () => {
+        const { getByTestId } = render(<Square letter='A' selected={false}/>);
+        expect(getByTestId('square'))
+    });
+
+    it('displays a selected square', () => {
+        const { getByTestId } = render(<Square letter='A' selected={true} />);
+        expect(getByTestId('square-selected'))
     })
 });
