48e2699285c32ad1c18822b7a19c71c11103c341
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-06-30 20:14:39 +0100

    Install a bunch of testing tools and slightly refactor InputField

--- src/InputField/InputField.js
+++ src/InputField/InputField.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export const InputField = ({onInput}) => {
+    const inputRef = useRef();
+
   return (
       <form className={'inputField'}>
-          <input className={'input'} id='input' type="text" onInput={() => onInput(document.getElementById('input').value)} />
+          <input className={'input'} id='input' type="text" ref={inputRef} onChange={() => onInput(inputRef.current.value)} />
       </form>
   )
 };
