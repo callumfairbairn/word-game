c1198788ccb4ebf31bbccc6e8c59720eb3512842
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-10-25 17:23:55 +0100

    Update fixPreviousEntries to accept an array as adjacentLetters

---
+++ src/common/fixPreviousEntries.js
@@ -0,0 +1,11 @@
+import {areLettersAdjacent} from "./areLettersAdjacent";
+
+const fixPreviousEntries = (grid, previousLetters, adjacentLetters) => {
+    return previousLetters.filter((letter) => {
+        return adjacentLetters.map(adjacentLetter => {
+            return areLettersAdjacent(letter, adjacentLetter)
+        }).includes(true)
+    });
+};
+
+export default fixPreviousEntries
--- src/common/fixPreviousEntry.js
+++
@@ -1,9 +0,0 @@
-import {areLettersAdjacent} from "./areLettersAdjacent";
-
-const fixPreviousEntry = (grid, previousLetters, adjacentLetter) => {
-    return previousLetters.filter((letter) => {
-        return areLettersAdjacent(letter, adjacentLetter)
-    });
-};
-
-export default fixPreviousEntry
