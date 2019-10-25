0a1946c23b4717052c3f5fe99c9185cc2d720084
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-10-25 23:18:25 +0100

    Implement pathFindingAlgorithm to work out which letters are selected

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -2,12 +2,13 @@ import React from "react";
 import './Grid.scss'
 import {Square} from "../Square/Square";
 import {xDim, yDim} from "../common/constants";
-import { forGridAndInputExecuteFunction, generateGrid } from "../common/functions";
+import {generateGrid} from "../common/functions";
+import pathFindingAlgorithm from "../common/pathFindingAlgorithm";
 
 export const Grid = ({letterList, input}) => {
     const grid = generateGrid(letterList);
-    calculateSelectedLetters(grid, input);
-    getRidOfLoneSelectedLetters(grid, input);
+    const paths = pathFindingAlgorithm(grid, input);
+    const assignedGrid = assignLetterStatus(grid, paths);
 
     return (
         <div className='grid' key={input} >
@@ -15,7 +16,7 @@ export const Grid = ({letterList, input}) => {
                 <div className='row' key={y}>
                     {Array.from(Array(xDim), (_,x) =>
                         <div className='column' key={x}>
-                            <Square key={[x, y]} status={!(grid[x][y].inputIndex === null) ? 'selected' : ''} letter={grid[x][y].letter} />
+                            <Square key={[x, y]} status={!(assignedGrid[x][y].inputIndex === null) ? 'selected' : ''} letter={assignedGrid[x][y].letter} />
                         </div>
                     )}
                 </div>
@@ -24,66 +25,21 @@ export const Grid = ({letterList, input}) => {
     )
 };
 
-const calculateSelectedLetters = (grid, input) => {
-    const func = (grid, input, i, x, y) => {
-        if (grid[x][y].letter.toLowerCase() === input[i] &&
-            grid[x][y].inputIndex === null &&
-            ((i === 0) ? true : isIndexAdjacent(grid,  x ,y, i-1))
-        ) {
-            grid[x][y].inputIndex = i;
-        }
-    };
-    forGridAndInputExecuteFunction(grid, input, func)
-};
-
-const isIndexAdjacent = (grid, x, y, index) => {
-    for (let m = -1; m < 2; m++) {
-        for (let n = -1; n < 2; n++) {
-            if (0 <= x+m  && x+m <= xDim-1 && 0 <= y+n  && y+n <= yDim-1 && grid[x+m][y+n].inputIndex === index) {
-                return true;
-            }
-        }
-    }
-    return false;
-};
-
-const getRidOfLoneSelectedLetters = (grid, input) => {
-    // This gets rid of all selected letters if a wrong letter is typed in because the last letter cannot be placed and
-    // therefore no letter can be adjacent to that incorrect letter.
-    // Also, if a letter is a valid duplicate, all letters will be unselected because calculateSelectedLetters only
-    // places the next inputIndex if the letter has a null input index. Therefore, the inputIndex corresponding to the
-    // last letter of the input will never be in the grid.
-    const func = (grid, input, i, x, y) => {
-        if (grid[x][y].inputIndex < input.length-1 &&
-            !isIndexAdjacent(grid, x, y, grid[x][y].inputIndex+1)) {
-            grid[x][y].inputIndex = null
-        }
-    };
-    forGridAndInputExecuteFunction(grid, input, func)
-};
-
-export const returnPositionsOfAdjacentGivenLetters = (grid, x, y, letter) => {
-    const newListOfPositions = [];
-    if (letter) {
-        for (let m = -1; m < 2; m++) {
-            for (let n = -1; n < 2; n++) {
-                if (0 <= x+m  && x+m <= xDim-1 && 0 <= y+n  && y+n <= yDim-1 && grid[x+m][y+n].letter.toLowerCase() === letter) {
-                    newListOfPositions.push({x: x+m, y: y+n});
+export const assignLetterStatus = (grid, paths) => {
+    if (paths.length > 0) {
+        paths.map(path => {
+            path.map((letter, i) => {
+                if (Array.isArray(letter)) {
+                    letter.map((_, j) => {
+                        console.log(letter, paths);
+                        grid[letter[j].x][letter[j].y].inputIndex = 1;
+                    })
+                } else {
+                    grid[path[i].x][path[i].y].inputIndex = 1;
                 }
-            }
-        }
+            })
+        })
     }
-    return newListOfPositions
-};
 
-export const findLocationsOfLetter = (grid, letter) => {
-    const locationList = [];
-    for (let x = 0; x < xDim; x++) {
-        for (let y = 0; y < yDim; y++) {
-            if (grid[x][y].letter.toLowerCase() === letter.toLowerCase()) {
-                locationList.push({x: x, y: y})
-            }
-        }
-    }
-    return locationList
+    return grid
 };
--- src/Grid/Grid.test.js
+++ src/Grid/Grid.test.js
@@ -1,8 +1,9 @@
 import {render} from "@testing-library/react";
-import {Grid} from "./Grid";
+import {assignLetterStatus, Grid} from "./Grid";
 import React from "react";
 import '../reactTestSetup'
 import {defaultLetterList} from "../common/constants";
+import {generateGrid} from "../common/functions";
 
 describe('Grid', () => {
     it('should render a grid of letters', () => {
@@ -20,14 +21,71 @@ describe('Grid', () => {
         expect(queryAllByTestId('square-selected').length).toEqual(0)
     });
 
-    it('should not select a letter if it has already been selected earlier', () => {
-        const {queryAllByTestId} = render(<Grid letterList={defaultLetterList} input={'olpok'}/>);
-        expect(queryAllByTestId('square-selected').length).toEqual(0)
-    });
-
     it('should not keep a letter selected if the next letter is not adjacent to it', () => {
         const customLetterList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "A", "N", "O", "P"];
         const {queryAllByTestId} = render(<Grid letterList={customLetterList} input={'ab'}/>);
         expect(queryAllByTestId('square-selected').length).toEqual(2)
     })
+});
+
+describe('assignLetterStatus', () => {
+    it('assigns no letters status if path is []', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [];
+        const result = assignLetterStatus(grid, paths);
+        result.map(x => {
+            x.map(y => {
+                expect(y.inputIndex).toEqual(null)
+            })
+        })
+    });
+
+    it('assigns letter status to one letter', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [[{ x: 0 , y: 0 }]];
+        const result = assignLetterStatus(grid, paths);
+        expect(result[0][0].inputIndex).toEqual(1)
+    });
+
+    it('assigns letter status to a different letter', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [[{ x: 1 , y: 1 }]];
+        const result = assignLetterStatus(grid, paths);
+        expect(result[1][1].inputIndex).toEqual(1)
+    });
+
+    it('assigns status for two letters', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [[{ x: 1 , y: 1 }, { x: 2 , y: 2 }]];
+        const result = assignLetterStatus(grid, paths);
+        expect(result[1][1].inputIndex).toEqual(1);
+        expect(result[2][2].inputIndex).toEqual(1)
+    });
+
+    it('assigns status for a branching path', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [[{ x: 1 , y: 2 }, [ { x: 3 , y: 1 }, { x: 0 , y: 2 }]]];
+        const result = assignLetterStatus(grid, paths);
+        expect(result[1][2].inputIndex).toEqual(1);
+        expect(result[3][1].inputIndex).toEqual(1);
+        expect(result[0][2].inputIndex).toEqual(1)
+    });
+
+    it('assigns status for multiple paths', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [[{ x: 1 , y: 1 }, { x: 2 , y: 1 }], [{ x: 2 , y: 2 }, { x: 2 , y: 3 }]];
+        const result = assignLetterStatus(grid, paths);
+        expect(result[1][1].inputIndex).toEqual(1);
+        expect(result[2][1].inputIndex).toEqual(1);
+        expect(result[2][2].inputIndex).toEqual(1)
+    });
+
+    it('assigns status for multiple paths', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [[{ x: 1 , y: 1 }, { x: 2 , y: 1 }], [{ x: 2 , y: 2 }, { x: 2 , y: 3 }]];
+        const result = assignLetterStatus(grid, paths);
+        expect(result[1][1].inputIndex).toEqual(1);
+        expect(result[2][1].inputIndex).toEqual(1);
+        expect(result[2][2].inputIndex).toEqual(1)
+    });
 });
