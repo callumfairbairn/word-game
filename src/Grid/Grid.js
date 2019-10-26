ed61ce1d375bfc81d6ccb16d0c417eec82ce0880
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-10-26 14:08:24 +0100

    Allow assignLetterStatus to assign 'correct'

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -25,16 +25,17 @@ export const Grid = ({letterList, input}) => {
     )
 };
 
-export const assignLetterStatus = (grid, paths) => {
+export const assignLetterStatus = (grid, paths, inputValid) => {
+    const status = inputValid ? 'correct' : 'selected';
     if (paths.length > 0) {
         paths.map(path => {
             path.map((letter, i) => {
                 if (Array.isArray(letter)) {
                     letter.map((_, j) => {
-                        grid[letter[j].x][letter[j].y].status = 'selected';
+                        grid[letter[j].x][letter[j].y].status = status;
                     })
                 } else {
-                    grid[path[i].x][path[i].y].status = 'selected';
+                    grid[path[i].x][path[i].y].status = status;
                 }
             })
         })
--- src/Grid/Grid.spec.js
+++ src/Grid/Grid.spec.js
@@ -12,7 +12,7 @@ describe('Grid', () => {
     });
 
     it('should render five selected letters given an input of length 5', () => {
-        const {queryAllByTestId} = render(<Grid letterList={defaultLetterList} input={'knife'}/>);
+        const {queryAllByTestId} = render(<Grid letterList={defaultLetterList} input={'knifc'}/>);
         expect(queryAllByTestId('square-selected').length).toEqual(5)
     });
 
@@ -32,7 +32,8 @@ describe('assignLetterStatus', () => {
     it('assigns no letters status if path is []', () => {
         const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
         const paths = [];
-        const result = assignLetterStatus(grid, paths);
+        const inputValid = false;
+        const result = assignLetterStatus(grid, paths, inputValid);
         result.map(x => {
             x.map(y => {
                 expect(y.status).toEqual(null)
@@ -40,52 +41,71 @@ describe('assignLetterStatus', () => {
         })
     });
 
-    it('assigns letter status to one letter', () => {
+    it('assigns selected to one letter', () => {
         const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
         const paths = [[{ x: 0 , y: 0 }]];
-        const result = assignLetterStatus(grid, paths);
+        const inputValid = false;
+        const result = assignLetterStatus(grid, paths, inputValid);
         expect(result[0][0].status).toEqual('selected')
     });
 
-    it('assigns letter status to a different letter', () => {
+    it('assigns selected to a different letter', () => {
         const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
         const paths = [[{ x: 1 , y: 1 }]];
-        const result = assignLetterStatus(grid, paths);
+        const inputValid = false;
+        const result = assignLetterStatus(grid, paths, inputValid);
         expect(result[1][1].status).toEqual('selected')
     });
 
-    it('assigns status for two letters', () => {
+    it('assigns selected for two letters', () => {
         const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
         const paths = [[{ x: 1 , y: 1 }, { x: 2 , y: 2 }]];
-        const result = assignLetterStatus(grid, paths);
+        const inputValid = false;
+        const result = assignLetterStatus(grid, paths, inputValid);
         expect(result[1][1].status).toEqual('selected');
         expect(result[2][2].status).toEqual('selected')
     });
 
-    it('assigns status for a branching path', () => {
+    it('assigns selected for a branching path', () => {
         const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
         const paths = [[{ x: 1 , y: 2 }, [ { x: 3 , y: 1 }, { x: 0 , y: 2 }]]];
-        const result = assignLetterStatus(grid, paths);
+        const inputValid = false;
+        const result = assignLetterStatus(grid, paths, inputValid);
         expect(result[1][2].status).toEqual('selected');
         expect(result[3][1].status).toEqual('selected');
         expect(result[0][2].status).toEqual('selected')
     });
 
-    it('assigns status for multiple paths', () => {
+    it('assigns selected for multiple paths', () => {
         const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
         const paths = [[{ x: 1 , y: 1 }, { x: 2 , y: 1 }], [{ x: 2 , y: 2 }, { x: 2 , y: 3 }]];
-        const result = assignLetterStatus(grid, paths);
+        const inputValid = false;
+        const result = assignLetterStatus(grid, paths, inputValid);
         expect(result[1][1].status).toEqual('selected');
         expect(result[2][1].status).toEqual('selected');
         expect(result[2][2].status).toEqual('selected')
     });
 
-    it('assigns status for multiple paths', () => {
+    it('assigns selected for multiple paths', () => {
         const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
         const paths = [[{ x: 1 , y: 1 }, { x: 2 , y: 1 }], [{ x: 2 , y: 2 }, { x: 2 , y: 3 }]];
-        const result = assignLetterStatus(grid, paths);
+        const inputValid = false;
+        const result = assignLetterStatus(grid, paths, inputValid);
         expect(result[1][1].status).toEqual('selected');
         expect(result[2][1].status).toEqual('selected');
         expect(result[2][2].status).toEqual('selected')
     });
+
+    it('assigns correct for a correct word', () => {
+        const grid = generateGrid(["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P"]);
+        const paths = [[{ x: 2 , y: 2 }, { x: 3 , y: 1 }, { x: 2 , y: 0 }, { x: 1 , y: 1 }, { x: 1 , y: 0 }]];
+        const inputValid = true;
+        const result = assignLetterStatus(grid, paths, inputValid);
+        expect(result[2][2].status).toEqual('correct');
+        expect(result[3][1].status).toEqual('correct');
+        expect(result[2][0].status).toEqual('correct');
+        expect(result[1][1].status).toEqual('correct');
+        expect(result[1][0].status).toEqual('correct')
+
+    })
 });
