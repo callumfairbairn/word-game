5576bbf5e537bf6a038b795e054a727d8bb6fcbd
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-10-27 14:13:48 +0000

    Prevent page from refreshing then enter is pressed

--- src/InputField/InputField.js
+++ src/InputField/InputField.js
@@ -4,8 +4,17 @@ export const InputField = ({onInput}) => {
     const inputRef = useRef();
 
   return (
-      <form className={'inputField'} id={'input-form'}>
-          <input className={'input'} id='input' type="text" ref={inputRef} onChange={() => onInput(inputRef.current.value.toLowerCase())} />
+      <form className={'inputField'} id={'input-form'} onSubmit={preventSubmit}>
+          <input className={'input'}
+                 id='input'
+                 type='text'
+                 ref={inputRef}
+                 onChange={() => onInput(inputRef.current.value.toLowerCase())}
+          />
       </form>
   )
+};
+
+const preventSubmit = (e) => {
+    e.preventDefault()
 };
