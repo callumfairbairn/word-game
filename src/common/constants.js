0ba96be8eb043410168cdef952d7573591c96d25
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-11-02 18:10:35 +0000

    Only allow grids with five or more vowels

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,5 @@
 export const alphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 export const defaultLetterList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+export const vowels = ['A', 'E', 'I', 'O', 'U']
 export const xDim = 4
 export const yDim = 4
--- src/common/functions.js
+++ src/common/functions.js
@@ -1,13 +1,24 @@
-import { alphabetString, defaultLetterList, xDim, yDim } from './constants'
+import { alphabetString, defaultLetterList, xDim, yDim, vowels } from './constants'
 
 const generateRandomLetter = () => {
   return alphabetString[Math.floor(Math.random() * 26)]
 }
 
 export const generateRandomLetterList = () => {
-  return Array.from(Array(xDim * yDim), () => {
-    return generateRandomLetter()
-  })
+  let valid = false
+  let letterList
+  while (!valid) {
+    letterList = Array.from(Array(xDim * yDim), () => {
+      return generateRandomLetter()
+    })
+
+    const vowelNumber = letterList.filter(letter => vowels.indexOf(letter) !== -1).length
+    if (vowelNumber > 4) {
+      valid = true
+    }
+  }
+
+  return letterList
 }
 
 export const generateGrid = (letterList = defaultLetterList) => {
