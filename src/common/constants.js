655a2d46cc280d5f15d2c10db4d3c03a03efc103
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-12-10 22:37:21 +0000

    Slightly improve grid generation algorithm by filtering out boards that have more than two hard letters

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,7 @@
 export const ALPHABET_STRING = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 export const DEFAULT_LETTER_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
 export const VOWELS = ['A', 'E', 'I', 'O', 'U']
+export const HARD_LETTERS = ['J', 'Q', 'X', 'Z']
 export const X_DIM = 4
 export const Y_DIM = 4
 export const STARTING_TIME = {
--- src/functions/Generation/generation.js
+++ src/functions/Generation/generation.js
@@ -1,4 +1,4 @@
-import { ALPHABET_STRING, DEFAULT_LETTER_LIST, X_DIM, Y_DIM, VOWELS } from '../../common/constants'
+import { ALPHABET_STRING, DEFAULT_LETTER_LIST, X_DIM, Y_DIM, VOWELS, HARD_LETTERS } from '../../common/constants'
 
 const generateRandomLetter = () => {
   return ALPHABET_STRING[Math.floor(Math.random() * 26)]
@@ -13,7 +13,11 @@ export const generateRandomLetterList = () => {
     })
 
     const vowelNumber = letterList.filter(letter => VOWELS.indexOf(letter) !== -1).length
-    if (vowelNumber > 4) {
+    const hardLetterNumber = letterList.filter(letter => HARD_LETTERS.indexOf(letter) !== -1).length
+    if (
+      vowelNumber > 4 &&
+        hardLetterNumber < 3
+    ) {
       valid = true
     }
   }
