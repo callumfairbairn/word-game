50492118b88c150a819a1cc1c70dd98545790fee
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-12-21 17:24:43 +0000

    Make constants easier to read

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,13 +1,18 @@
+// Determine size of grid
+export const X_DIM = 4
+export const Y_DIM = 4
+
+// Letter list generation constants
 export const ALPHABET_STRING = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 export const ALPHABET_SET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 export const DEFAULT_LETTER_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
 export const VOWELS = ['A', 'E', 'I', 'O', 'U']
 export const CONSONANTS = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
 export const HARD_LETTERS = ['J', 'Q', 'X', 'Z']
-export const X_DIM = 4
-export const Y_DIM = 4
 export const MIN_NUMBER_OF_VOWELS = 6
 export const MAX_NUMBER_OF_HARD_LETTERS = 3
+
+// Set time on timer at start of game
 export const STARTING_TIME = {
   minutes: 2,
   seconds: 0
