15b4cf53708b07676b6e7f7e751fbd66d7026f7b
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-12-21 16:52:36 +0000

    Move ensureConsonantsTouchAtLeastOneVowel's functions back into it's file

--- src/functions/LetterListGeneration/EnsureConsonantsTouchAtLeastOneVowel/ensureConsonantsTouchAtLeastOneVowel.js
+++ src/functions/LetterListGeneration/EnsureConsonantsTouchAtLeastOneVowel/ensureConsonantsTouchAtLeastOneVowel.js
@@ -1,8 +1,5 @@
 import { CONSONANTS, VOWELS } from '../../../common/constants'
-import { doesListContainVowel } from './doesListContainVowel'
-import { returnAdjacentLettersInList } from './returnAdjacentLettersInList'
 import { addLetterFromSet } from '../addAndTakeAwayLetterFromSet'
-import { putSetOfAdjacentLettersBackIn } from './putSetOfAdjacentLettersBackIn'
 
 export const ensureConsonantsTouchAtLeastOneVowel = (letterList) => {
   letterList.map((letter, position) => {
@@ -19,6 +16,69 @@ export const ensureConsonantsTouchAtLeastOneVowel = (letterList) => {
 
         putSetOfAdjacentLettersBackIn(adjacentLetters, position, letterList)
       }
+    }
+  })
+}
+
+export const returnAdjacentLettersInList = (letterList, position) => {
+  const adjacentLetters = []
+  if (letterList[position - 4]) {
+    adjacentLetters.push(
+      {
+        letter: letterList[position - 4],
+        direction: 'up'
+      }
+    )
+  }
+  if (position % 4 < (position + 1) % 4) {
+    adjacentLetters.push(
+      {
+        letter: letterList[position + 1],
+        direction: 'right'
+      }
+    )
+  }
+  if (letterList[position + 4]) {
+    adjacentLetters.push(
+      {
+        letter: letterList[position + 4],
+        direction: 'down'
+      }
+    )
+  }
+  if (position % 4 !== 0) {
+    adjacentLetters.push(
+      {
+        letter: letterList[position - 1],
+        direction: 'left'
+      }
+    )
+  }
+  return adjacentLetters
+}
+
+export const doesListContainVowel = (letterList) => {
+  for (let x = 0; x < letterList.length; x++) {
+    if (VOWELS.includes(letterList[x])) {
+      return true
+    }
+  }
+  return false
+}
+
+export const putSetOfAdjacentLettersBackIn = (adjacentLetters, position, letterList) => {
+  adjacentLetters.map((adjacentLetter) => {
+    if (adjacentLetter.direction === 'up') {
+      letterList[position - 4] = adjacentLetter.letter
+    }
+    if (adjacentLetter.direction === 'right') {
+      letterList[position + 1] = adjacentLetter.letter
+    }
+    if (adjacentLetter.direction === 'down') {
+      letterList[position + 4] = adjacentLetter.letter
+    }
+    if (adjacentLetter.direction === 'left') {
+      letterList[position - 1] = adjacentLetter.letter
     }
   })
 }
--- src/functions/LetterListGeneration/EnsureConsonantsTouchAtLeastOneVowel/ensureConsonantsTouchAtLeastOneVowel.spec.js
+++ src/functions/LetterListGeneration/EnsureConsonantsTouchAtLeastOneVowel/ensureConsonantsTouchAtLeastOneVowel.spec.js
@@ -1,12 +1,14 @@
 import { DEFAULT_LETTER_LIST, VOWELS } from '../../../common/constants'
-import { ensureConsonantsTouchAtLeastOneVowel } from './ensureConsonantsTouchAtLeastOneVowel'
-import { returnAdjacentLettersInList } from './returnAdjacentLettersInList'
-import { doesListContainVowel } from './doesListContainVowel'
+import {
+  doesListContainVowel,
+  ensureConsonantsTouchAtLeastOneVowel,
+  putSetOfAdjacentLettersBackIn, returnAdjacentLettersInList
+} from './ensureConsonantsTouchAtLeastOneVowel'
 import { runAssertionOneHundredTimes } from '../../RunAssertionOneHundredTimes/runAssertionOneHundredTimes'
 
 describe('ensureConsonantsTouchAtLeastOneVowel', () => {
   it('puts a vowel next to H in the default letterList or turns H into vowel', () => {
-    const letterList = DEFAULT_LETTER_LIST
+    const letterList = [...DEFAULT_LETTER_LIST]
     ensureConsonantsTouchAtLeastOneVowel(letterList)
     const adjacentLettersToH = returnAdjacentLettersInList(letterList, 7)
 
@@ -17,5 +19,80 @@ describe('ensureConsonantsTouchAtLeastOneVowel', () => {
       ).toEqual(true)
     }
     runAssertionOneHundredTimes(assertion)
+  })
+})
+
+describe('returnAdjacentLettersInList', () => {
+  it('returns above, below, left and right of given letter', () => {
+    const letterList = DEFAULT_LETTER_LIST
+    const position = 5
+    expect(returnAdjacentLettersInList(letterList, position)).toEqual([
+      { letter: 'B', direction: 'up' },
+      { letter: 'G', direction: 'right' },
+      { letter: 'J', direction: 'down' },
+      { letter: 'E', direction: 'left' }
+    ])
+  })
+
+  it('returns right and below if given top left corner of grid', () => {
+    const letterList = DEFAULT_LETTER_LIST
+    const position = 0
+    expect(returnAdjacentLettersInList(letterList, position)).toEqual([
+      { letter: 'B', direction: 'right' },
+      { letter: 'E', direction: 'down' }
+    ])
+  })
+
+  it('returns above and left if given bottom right corner of grid', () => {
+    const letterList = DEFAULT_LETTER_LIST
+    const position = 15
+    expect(returnAdjacentLettersInList(letterList, position)).toEqual([
+      { letter: 'L', direction: 'up' },
+      { letter: 'O', direction: 'left' }
+    ])
+  })
+
+  it('returns above, below and left if letter is on right edge', () => {
+    const letterList = DEFAULT_LETTER_LIST
+    const position = 7
+    expect(returnAdjacentLettersInList(letterList, position)).toEqual([
+      { letter: 'D', direction: 'up' },
+      { letter: 'L', direction: 'down' },
+      { letter: 'G', direction: 'left' }
+    ])
+  })
+
+  it('returns above, below and right if letter is on left edge', () => {
+    const letterList = DEFAULT_LETTER_LIST
+    const position = 4
+    expect(returnAdjacentLettersInList(letterList, position)).toEqual([
+      { letter: 'A', direction: 'up' },
+      { letter: 'F', direction: 'right' },
+      { letter: 'I', direction: 'down' }
+    ])
+  })
+})
+
+describe('doesListContainVowel', () => {
+  it('returns false for a list not containing a vowel', () => {
+    const letterList = ['X', 'Y', 'Z']
+    expect(doesListContainVowel(letterList)).toEqual(false)
+  })
+
+  it('returns true for a list containing a vowel', () => {
+    const letterList = ['X', 'O', 'Z']
+    expect(doesListContainVowel(letterList)).toEqual(true)
+  })
+})
+
+describe('putSetOfAdjacentLettersBackIn', () => {
+  it('changes the original letterList', () => {
+    const letterList = DEFAULT_LETTER_LIST
+    const position = 0
+    const adjacentLetters = [{ letter: 'X', direction: 'right' }, { letter: 'Y', direction: 'down' }]
+
+    putSetOfAdjacentLettersBackIn(adjacentLetters, position, letterList)
+
+    expect(letterList).toEqual(['A', 'X', 'C', 'D', 'Y', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'])
   })
 })
