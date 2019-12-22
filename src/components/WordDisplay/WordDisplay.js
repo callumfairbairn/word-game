71928ac82d01a3762463be1451d8aa871c55639c
callumfairbairn <callum.fairbairn@thoughtworks.com>
2019-12-22 14:08:38 +0000

    Word display styling

--- src/components/WordDisplay/WordDisplay.js
+++ src/components/WordDisplay/WordDisplay.js
@@ -5,7 +5,9 @@ const WordDisplay = ({ foundWords }) => {
   return (
     <div className='word-display' data-testid='word-display'>
       <div className='number-of-words'>
-        {foundWords.length}
+        <div>
+          {foundWords.length}
+        </div>
       </div>
       <div className='found-words'>
         {foundWords.map(word => { return <div key={word}>{word}</div> })}
