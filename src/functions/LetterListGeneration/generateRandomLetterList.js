34b9a913f3e3463db017287f241f0a195fed89fe
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-01-11 17:41:13 +0000

    Add constant containing number of times a letter can appear (before modifications)

--- src/functions/LetterListGeneration/generateRandomLetterList.js
+++ src/functions/LetterListGeneration/generateRandomLetterList.js
@@ -4,11 +4,11 @@ import { reduceNumberOfHardLetters } from './reduceNumberOfHardLetters/reduceNum
 import { increaseNumberOfVowels } from './increaseNumberOfVowels/increaseNumberOfVowels'
 import { increaseOccurrencesOfS } from './increaseOccurancesOfS/increaseOccurrencesOfS'
 import { ensureQHasAdjacentU } from './ensureQHasAdjacentU/ensureQHasAdjacentU'
+import { returnNumberOfTimesSetAppearsInList } from './common/returnNumberOfTimesSetAppearsInList'
+import { letterOccurrenceFrequencies } from '../../common/letterOccurranceFrequencies'
 
 export const generateRandomLetterList = () => {
-  const letterList = Array.from(Array(X_DIM * Y_DIM), () => {
-    return generateRandomLetter()
-  })
+  const letterList = generateBaseList()
 
   reduceNumberOfHardLetters(letterList)
   ensureConsonantsTouchAtLeastOneVowel(letterList)
@@ -20,4 +20,21 @@ export const generateRandomLetterList = () => {
 
 const generateRandomLetter = () => {
   return ALPHABET_STRING[Math.floor(Math.random() * 26)]
+}
+
+const generateBaseList = () => {
+  const list = []
+  for (let x = 0; x < X_DIM * Y_DIM; x++) {
+    let foundValidLetter = false
+
+    while (!foundValidLetter) {
+      const letter = generateRandomLetter()
+      if (returnNumberOfTimesSetAppearsInList(list, [letter]) < letterOccurrenceFrequencies[letter]) {
+        list.push(letter)
+        foundValidLetter = true
+      }
+    }
+  }
+
+  return list
 }
