0b8ef3a4b7355060f947fb04c4512f1f56c3d25b
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-01-17 13:09:50 +0000

    Get rid of not statement in calculateWordStatus

--- src/functions/WordValidation/calculateWordStatus.js
+++ src/functions/WordValidation/calculateWordStatus.js
@@ -1,8 +1,8 @@
 const calculateWordStatus = (word, dict, foundWords, userHasPressedReturn) => {
   if (word.length < 3) return userHasPressedReturn ? 'wrong' : 'selected'
   if (foundWords.indexOf(word) !== -1) return 'found'
-  if (!userHasPressedReturn) return 'selected'
-  return dict.words.indexOf(word) !== -1 ? 'correct' : 'wrong'
+  if (userHasPressedReturn) return dict.words.indexOf(word) !== -1 ? 'correct' : 'wrong'
+  return 'selected'
 }
 
 export default calculateWordStatus
