55a10c0a6fee1986879c3dad0ef1aa29df4eaee8
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-02-12 08:59:20 +0000

    HeatMap styling

--- src/functions/AssignLetterStatus/assignHeatMapStatus.js
+++ src/functions/AssignLetterStatus/assignHeatMapStatus.js
@@ -1,23 +1,22 @@
 export const assignHeatMapStatus = (grid, heatMap) => {
-  let max = 0; let min = 0
+  let max = 0
+  const minBoundary = 0.2
+  const maxBoundary = 0.8
 
   heatMap.forEach(row => row.forEach(value => {
     if (value > max) {
       max = value
     }
 
-    if (value < min) {
-      min = value
-    }
   }))
 
   return grid.map((row, x) => row.map((letter, y) => {
     const newLetter = letter
 
-    if (heatMap[x][y] <= min) {
-      newLetter.status = 0.1
-    } else if (heatMap[x][y] >= max) {
-      newLetter.status = 0.9
+    if (heatMap[x][y] <= max * minBoundary) {
+      newLetter.status = minBoundary
+    } else if (heatMap[x][y] >= max * maxBoundary) {
+      newLetter.status = maxBoundary
     } else {
       newLetter.status = Math.floor(100 * heatMap[x][y] / max) / 100
     }
