eadf07010524998b319b478c58670cc0b6d4a7a7
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-06-01 11:29:38 +0100

    Add findPossibleWords function

---
+++ src/functions/FindWords/findPossibleWords.spec.js
@@ -0,0 +1,21 @@
+import {findPossibleWords} from "./findPossibleWords";
+import {expectArrayToContainInAnyOrder} from "../../testHelpers/ExpectArrayToContainInAnyOrder/expectArrayToContainInAnyOrder";
+
+const dict = require('../../words')
+
+describe('findPossibleWords', () => {
+  it('returns empty string if input is not the start of any word', () => {
+    const startOfWord = 'ljkhsdf'
+    expectArrayToContainInAnyOrder(findPossibleWords(startOfWord, dict), [])
+  })
+
+  it('returns the input if the input is in the dictionary', () => {
+    const startOfWord = 'catamarans'
+    expectArrayToContainInAnyOrder(findPossibleWords(startOfWord, dict), ['catamarans'])
+  })
+
+  it('returns all words that start with the input string', () => {
+    const startOfWord = 'catamaran'
+    expectArrayToContainInAnyOrder(findPossibleWords(startOfWord, dict), ['catamaran', 'catamarans'])
+  })
+})
