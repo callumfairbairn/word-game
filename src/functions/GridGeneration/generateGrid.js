911047d281e8f6fe6336379aff8344362ae09c3b
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-06-01 14:13:14 +0100

    Add location as a property of each letter in a grid

--- src/functions/GridGeneration/generateGrid.js
+++ src/functions/GridGeneration/generateGrid.js
@@ -2,12 +2,13 @@ import { DEFAULT_LETTER_LIST, X_DIM, Y_DIM } from '../../common/constants'
 
 export const generateGrid = (letterList = DEFAULT_LETTER_LIST) => {
   let i = -1
-  return Array.from(Array(X_DIM), () => {
-    return Array.from(Array(Y_DIM), () => {
+  return Array.from(Array(X_DIM), (_, x) => {
+    return Array.from(Array(Y_DIM), (_, y) => {
       i++
       return {
         letter: letterList[i],
-        status: null
+        status: null,
+        location: { x: x , y: y }
       }
     })
   })
