1d3de5278717b1290e7f1e0d3cadfa57f6d44581
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-06-04 11:47:49 +0100

    Add logging to generate_letter_list.py and move everything into functions

--- python/generate_letter_list.py
+++ python/generate_letter_list.py
@@ -1,13 +1,41 @@
 import json
+import logging
 import os
 
 
-if not os.path.isdir('../generated'):
-    os.makedirs('../generated')
+def setup_logging() -> logging.Logger:
+    logging.basicConfig(format='[%(asctime)s]:%(message)s')
+    new_logger = logging.getLogger('logger')
+    new_logger.setLevel(20)
+    return new_logger
 
-output = {'letter_list': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']}
 
-with open('../generated/letter_list.json', 'w') as output_file:
-    json.dump(output, output_file)
-    output_file.close()
+logger = setup_logging()
+logger.info('Running generate_letter_list.py...')
 
+
+def create_folder(folder_path: str):
+    if not os.path.isdir(folder_path):
+        logger.info('generated folder does not exist, creating...')
+        os.makedirs(folder_path)
+    else:
+        logger.info('generated folder already exists, skipping...')
+
+
+def write_output(output_path: str, output: dict):
+    with open(output_path, 'w') as output_file:
+        logger.info('outputting %s to %s', output, output_path)
+        json.dump(output, output_file)
+        output_file.close()
+
+
+def main():
+    folder_path = '../generated'
+    output_path = '../generated/letter_list.json'
+
+    create_folder(folder_path)
+    output = {'letter_list': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']}
+    write_output(output_path, output)
+
+
+main()
