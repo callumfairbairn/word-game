e9ed8671012fa1d53e5156584ea63aea9a2525d6
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-07-07 12:07:54 +0100

    react: Use found_words fom api response

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react'
 import './App.scss'
-import { generateGrid } from './functions/GridGeneration/generateGrid'
-import { findWords } from './functions/FindWords/findWords'
 import { Game } from './components/Game/Game'
 
 const App = (data) => {
-  const dict = require('./words')
-
   const [letterList, setLetterList] = useState(data.data['letter_list'])
 
-  const possibleWords = findWords(generateGrid(letterList), dict)
+  const possibleWords = data.data['found_words']
 
   return (
     <div className='App'>
